b5a135b425973df78430f1fee599a128d0fc3812
sparten9999 <kissrock247@aol.com>
2018-03-21 16:10:45 -0400

    add list maker
    included visual studio 2017 program with rant to create list of groups

---
+++ list maker/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Rant;
+
+namespace ConsoleApp1
+{
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rant = new RantEngine();
+            rant.LoadPackage("Rantionary-3.0.20.rantpkg");
+            rant.Dictionary.IncludeHiddenClass("nsfw");
+
+            Console.WriteLine("Press <Enter> to create a list of 1000 groups");
+            Console.ReadKey(true);
+            Console.Clear();
+
+            var pgm = RantProgram.CompileString(@"[rs:1000;\n]{<adj> <adj> <noun.pl>}");
+            var output = rant.Do(pgm);
+            Console.WriteLine(output.Main);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
